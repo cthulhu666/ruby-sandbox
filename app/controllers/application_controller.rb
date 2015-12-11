3597cc34234b9abda4645b374355aafb02dcb194
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2015-12-11 15:07:36 +0100

    protect_from_forgery with: :null_session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception if Rails.env.production?
+  protect_from_forgery with: :null_session
 
   rescue_from Timeout::Error do |e|
     render json: {error: 'Request timeout'}, status: 408
